505af12a4e144d5484334564630a4ff2fdb19bfa
Mbarek Benraiss <81750067+malika-1989@users.noreply.github.com>
2026-05-14 17:26:18 +0200

    test Antigravity IDE

--- currency-conversion-service/currency-conversion-service/src/main/java/com/mbarekDev/currency_conversion_service/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/mbarekDev/currency_conversion_service/CurrencyConversionServiceApplication.java
@@ -10,6 +10,8 @@ public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+		System.out.println("application started....... 2");
+
 	}
 
 }
